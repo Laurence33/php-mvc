20348ada3c83c214856c72f8322f2f719a5d55eb
Laurence Cortez <laurencecortez.com>
2023-05-13 15:28:57 +0800

    handle create note request

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,8 +1,16 @@
 <?php
+$config = require('config.php');
+$db = new Database($config['database']);
 
+$currentUserId = 1;
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    dd($_POST);
+    $params = [
+        ':body' => $_POST['body'],
+        ':user_id' => $currentUserId
+    ];
+    $note = $db->query("INSERT INTO note (body, user_id) VALUES(:body, :user_id);", $params);
 }
+
 
 $heading = 'Create Note';
 require 'views/note-create.view.php';
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -5,16 +5,16 @@
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
         <p>Create note page</p>
-        <form method="POST">
+        <form action="/notes/create" method="POST">
             <div class="space-y-12">
                 <div class="border-b border-gray-900/10 pb-12">
                     <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                         <div class="col-span-full">
                             <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
                             <div class="mt-2">
-                                <textarea id="body" name="about" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                                <textarea id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
                             </div>
-                            <p class="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
+                            <p class="mt-3 text-sm leading-6 text-gray-600">Tell us about the note you need to remember</p>
                         </div>
                     </div>
                 </div>
