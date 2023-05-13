69acb8d1f7716ce4d47bb88c7436ae2baae8761f
Laurence Cortez <laurencecortez.com>
2023-05-13 15:49:03 +0800

    simple validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -4,11 +4,24 @@ $db = new Database($config['database']);
 
 $currentUserId = 1;
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $params = [
-        ':body' => $_POST['body'],
-        ':user_id' => $currentUserId
-    ];
-    $note = $db->query("INSERT INTO note (body, user_id) VALUES(:body, :user_id);", $params);
+    $errors = [];
+
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'The body can not be more than 1000 characters.';
+    }
+
+    if (empty($errors)) {
+
+        $params = [
+            ':body' => $_POST['body'],
+            ':user_id' => $currentUserId
+        ];
+        $note = $db->query("INSERT INTO note (body, user_id) VALUES(:body, :user_id);", $params);
+    }
 }
 
 
