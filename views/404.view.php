33f14287066911be3529461055cc51031c30098f
Laurence Cortez <laurencecortez.com>
2023-05-13 09:00:32 +0800

    controller and views + Router

---
+++ views/404.view.php
@@ -0,0 +1,13 @@
+<?php require('partials/header.php') ?>
+<?php require 'partials/nav.php'; ?>
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <!-- Your content -->
+        <p class="text-2xl font-bold">Sorry, page not found.</p>
+
+        <p class="mt-4">
+            <a href="/" class="text-blue underline">Go back home.</a>
+        </p>
+    </div>
+</main>
+<?php require('partials/footer.php'); ?>
